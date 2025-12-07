f76ff4bbce7b241b78cf25902dc47ef136fef731
koinyan nelson <koinyannelson0@gmail.com>
2025-12-07 20:51:21 +0100

    implement home page with tradingView Widget

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -31,7 +31,7 @@ const UserDropdown = () => {
           className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
         >
           <Avatar className="h-8 w-8">
-            <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+            {/* <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" /> */}
             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
               {user.name[0]}
             </AvatarFallback>
@@ -47,7 +47,7 @@ const UserDropdown = () => {
         <DropdownMenuLabel>
           <div className="flex relative items-center gap-3 py-2">
             <Avatar className="h-10 w-10">
-              <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+              {/* <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" /> */}
               <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                 {user.name[0]}
               </AvatarFallback>
